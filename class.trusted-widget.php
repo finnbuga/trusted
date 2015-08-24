ee3a7c06a3017b2e33ecabd173809fefb78edd33
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2015-08-24 17:40:00 +0300

    Add comments for Trusted_Widget methods

--- class.trusted-widget.php
+++ class.trusted-widget.php
@@ -4,6 +4,9 @@ class Trusted_Widget extends WP_Widget {
   
   const VERIFICATION_URL = 'http://trusted.ro/assets/verify.php?id=';
 
+	/**
+	 * Register widget with WordPress.
+	 */
 	public function __construct() {
 
 		parent::__construct(
@@ -13,7 +16,13 @@ class Trusted_Widget extends WP_Widget {
 		);
 	}
 	
-	// Widget backend
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
 	public function form( $instance ) {
 		
 		// Input for widget title
@@ -39,14 +48,30 @@ class Trusted_Widget extends WP_Widget {
 <?php 
 	}
 	
-	// Sanitize widget form values as they are saved
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance['title']      = strip_tags( $new_instance['title'] );
 		$instance['trusted_id'] = strip_tags( $new_instance['trusted_id'] );
 		return $instance;
 	}
 
-	// Widget front-end
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
 	public function widget( $args, $instance ) {
 
 		// Before and after widget arguments are defined by themes
